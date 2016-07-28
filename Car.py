52433e3c08fbe7f9b8824142d0d6d8dd198775ff
T. Robert Ward <trironyard@icloud.com>
2016-07-28 15:11:48 -0400

    initial

---
+++ Car.py
@@ -0,0 +1,12 @@
+# car class
+
+class Car():
+    """     """
+    def __init__(self, val, carID, speed, accel_rate, location):
+        self.val = val
+        self.carID = carID
+        self.speed = speed
+        self.accel_rate = accel_rate
+        self.location = location
+
+    
---
+++ road_rage.py
@@ -0,0 +1,24 @@
+# W3D4 road_rage Project
+
+import Car
+
+
+#initialize cars
+for i in range(30)
+    car[i] = Car()
+
+print(type(car[0])
+
+
+# print them
+
+# update cars based on car's data
+    #accelerate & randomly slow
+
+# check for distance between each car
+
+# if too close - slow down
+# if gonna-hit, stop
+
+#==========
+    # position on road is 5 thru 1000
