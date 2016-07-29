2c96efbf0ed4f141859f367dca4072ba835adb8c
T. Robert Ward <trironyard@icloud.com>
2016-07-29 17:08:09 -0400

    finished scatter plot and std dev graph

--- road_rage.py
+++ road_rage.py
@@ -1,13 +1,59 @@
 #W3D4  road project
 
+import numpy as np
+import matplotlib.animation as animation
+import seaborn
+import statistics as st
 import random
+import matplotlib.pyplot as plt
+
 
 car = []
+diff_bt_cars = []
+speeds = []
+avg_speed = 0
+
 #initialize cars
 for i in range(30):
-    car.append([0,2,(12+(7*i))])                 # [ speed, accel, loc ]
+    car.append([0,2,(12+(7*i))])       # [ speed, accel, loc ]
 
 num_cars = len(car)
+
+
+def plot_it(x,y):
+    plt.title('Car Traffic Simulation (# of cars = 30)')
+    plt.xlabel('Distance bt Cars')
+    plt.ylabel('Velocity (km/h)')
+    plt.xlim((0, 100))
+    plt.ylim((0, 34))
+    plt.scatter(x, y, marker='s', cmap='set1', alpha=0.5)
+    plt.show()
+
+
+def plot_them():
+    for i in range(1,num_cars):
+        diff_bt_cars.append(car[i][2] - car[i-1][2] - 4)
+        speeds.append(car[i][0])
+        # avg_speed_list(avg_speed_ofeaccar, after acceleration period)
+        #
+        # Distance_traveled = 0
+
+
+def averages_graph(avgs_list):
+    plt.title('Average Speeds')
+    plt.hist(avgs_list)
+    plt.show()
+
+
+def std_dev(avgs_list):
+    plt.hist(avgs_list)
+    mean = st.mean(avgs_list)
+    stdev = st.stdev(avgs_list)
+    ymin, ymax = plt.ylim()
+
+    plt.vlines(mean, ymin, ymax, color='red')
+    plt.vlines([mean - 2 * stdev, mean - stdev, mean + stdev, mean + 2 * stdev], ymin, ymax, color='red', linestyles='dashed')
+    plt.show()
 
 
 def print_them():
@@ -29,6 +75,14 @@ def print_them():
     print('30'*3)
 
     input()
+
+
+def calc_averages(avg_speed):
+    for each in car:
+        avg_speed += int(each[0])
+        # print(each[0])
+    # input("enter")
+    return avg_speed/30
 
 
 # print them
@@ -70,7 +124,6 @@ def switch_car_positions():
     car[0][2] = 4
 
 
-
 def check_valid_position():
     if car[num_cars-1][2] > 999:
         switch_car_positions()
@@ -85,10 +138,31 @@ def random_slowdown():
                 each[0] = 0
 
 
-while True:
-    print_them()
+inital_acceleration_period = True
+loops = 0
+avg_speed_list = []
+
+while loops < 100:
+    loops += 1
+    if loops > 25:
+        inital_acceleration_period = False
+
+    # print_them()
+
+    if inital_acceleration_period == False:
+        avg_speed_list.append(calc_averages(avg_speed))
+        avg_speed = 0
+
     check_distance(5)
     update_car_speed()
     update_car_position()
     check_valid_position()
     random_slowdown()
+
+# plot_them()
+input("ENTER")
+print(sum(avg_speed_list))
+
+averages_graph(avg_speed_list)
+
+std_dev(avg_speed_list)
