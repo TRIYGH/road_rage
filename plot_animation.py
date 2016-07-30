65bdb8c01399d8e39a042315c63d3e0d6d58034f
B. Maxwell Ruotsi <bmruotsi@gmail.com>
2016-07-30 14:40:52 -0400

    scatter plot animation functioning

--- plot_animation.py
+++ plot_animation.py
@@ -1,52 +1,51 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import matplotlib.animation as animation
 import seaborn
 
-class Animate:
-
-    def init():
-        scatter.set_data([], [])
-        return scatter
-
-    def animate(i):
-        x = [car[2] for _ in car]
-        y = [car[0] for _ in car]
-        scatter.set_data(x, y)
-        return scatter
-
-    # def fig():
-
-
-    # call the animator.  blit=True means only re-draw the parts that have changed.
-    anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                   frames=200, interval=20, blit=False)
-
-
-    plt.title('Car Traffic Simulation (# of cars = 30)')
-    plt.xlabel('Distance traveled (m)')
-    plt.ylabel('Velocity (km/h)')
-    plt.xlim((0, 1000))
-    plt.ylim((0, 50))
-    plt.scatter(x, y, marker='s', cmap='set1', alpha=0.5)
-    plt.show()
-
-
-#     def main():
-#     numframes = 100
-#     numpoints = 10
-#     color_data = np.random.random((numframes, numpoints))
-#     x, y, c = np.random.random((3, numpoints))
+# class Animate:
 #
-#     fig = plt.figure()
-#     scat = plt.scatter(x, y, c=c, s=100)
+#     def init():
+#         scatter.set_data([], [])
+#         return scatter
 #
-#     ani = animation.FuncAnimation(fig, update_plot, frames=xrange(numframes),
-#                                   fargs=(color_data, scat))
-#     plt.show()
+#     def animate(i):
+#         x = [car[2] for _ in car]
+#         y = [car[0] for _ in car]
+#         scatter.set_data(x, y)
+#         return scatter
 #
-# def update_plot(i, data, scat):
-#     scat.set_array(data[i])
-#     return scat,
+#     # def fig():
 #
-# main()
+#
+#     # call the animator.  blit=True means only re-draw the parts that have changed.
+#     anim = animation.FuncAnimation(fig, main(), init_func=init,
+#                                    frames=200, interval=20, blit=False)
+
+
+x = [1, 2, 3]
+y = [1, 2, 3]
+
+fig, ax = plt.subplots()
+
+plt.title('Car Traffic Simulation (# of cars = 30)')
+plt.xlabel('Distance traveled (m)')
+plt.ylabel('Velocity (km/h)')
+plt.xlim((0, 1000))
+plt.ylim((0, 50))
+paths = ax.scatter(x, y, marker='s', cmap='set1', alpha=1)
+
+
+def animate(i):
+    x[0] += 0.5
+    x[1] += 0.55
+    x[2] += 0.1
+    y[0] += 0.51
+    y[1] += 0.5
+    y[2] += 0.1
+    points = list(zip(x, y))
+    paths.set_offsets(points)
+    return paths,
+
+
+ani = animation.FuncAnimation(fig, animate, interval=60)
+plt.show()
